0f5f9652b4f3f8b28803b36b38835ed65deec0df
HairutdinovEmmauel <hairutdinov.emmanuel@gmail.com>
2021-10-04 15:24:07 +0300

    Add MainLayout on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import Head from 'next/head';
 import HomePage from "../components/containers/HomePage";
+import MainLayout from '../components/layouts/MainLayout';
 
 const Home: React.FC = () => {
   return (
-    <>
+    <MainLayout>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomePage />
-    </>
+    </MainLayout>
   )
 }
 
