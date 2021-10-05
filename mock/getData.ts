910b612054084bffbdbb02a421e01003eb26ea5f
HairutdinovEmmauel <hairutdinov.emmanuel@gmail.com>
2021-10-05 08:56:09 +0300

    Create mock data for list products

--- mock/getData.ts
+++ mock/getData.ts
@@ -1,10 +1,12 @@
 import { GET_BRAND_PRODUCTS } from "./brandProducts";
 import { GET_CATEGORY_PRODUCTS } from "./categoryProducts";
+import { GET_PRODUCTS } from "./products";
 import { GET_SLIDER } from "./slider";
 
 export const getData = (data_type: string) => {
   switch(data_type) {
     case "GET_SLIDER": return GET_SLIDER;
+    case "GET_PRODUCTS": return GET_PRODUCTS;
     case "GET_BRAND_PRODUCTS": return GET_BRAND_PRODUCTS;
     case "GET_CATEGORY_PRODUCTS": return GET_CATEGORY_PRODUCTS;
     default : return {};
