8fdbf32d9194ce1c8ef0131ce9837ad92c13f191
HairutdinovEmmauel <hairutdinov.emmanuel@gmail.com>
2021-10-05 12:49:24 +0300

    Create mock data for list section products

---
+++ mock/sectionProducts.ts
@@ -0,0 +1,17 @@
+export const GET_SECTION_PRODUCTS = [
+  {
+    id: 1,
+    title: "Man",
+    imageUrl: "https://static.staff-clothes.com/uploads/media/default/0002/07/c3659f3910de4f5b87e76cd3631ac509.jpeg",
+  },
+  {
+    id: 2,
+    title: "Woman",
+    imageUrl: "https://static.staff-clothes.com/uploads/media/default/0002/07/c3659f3910de4f5b87e76cd3631ac509.jpeg",
+  },
+  {
+    id: 3,
+    title: "Child",
+    imageUrl: "https://static.staff-clothes.com/uploads/media/default/0002/07/c3659f3910de4f5b87e76cd3631ac509.jpeg",
+  }
+];
